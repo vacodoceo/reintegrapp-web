8b9167d37c600cbb0c2d6f687bd0404082ae0542
Verner Codoceo <vacodoceo@uc.cl>
2020-05-29 02:26:00 -0400

    Add linter, formatter, precommit, firebase and firebase hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import firebase from './firebase';
 
-function App() {
+const App = () => {
+  const [user, loading, error] = useAuthState(firebase.auth());
+
+  console.log(user, loading, error);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Test</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
